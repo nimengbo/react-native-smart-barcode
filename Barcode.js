3e7208a15887761db009870a950b2e51b1a81adf
Abner <nimengbo@gmail.com>
2018-06-30 13:50:21 +0800

    update prop types

--- Barcode.js
+++ Barcode.js
@@ -7,7 +7,6 @@
 
 
 import React, {
-    PropTypes,
     Component,
 } from 'react'
 import {
@@ -17,7 +16,7 @@ import {
     AppState,
     Platform,
 } from 'react-native'
-
+import PropTypes from 'prop-types'
 const BarcodeManager = Platform.OS == 'ios' ? NativeModules.Barcode : NativeModules.CaptureModule
 
 
